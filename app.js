72420d505b9857b5e7252c5e5295105c731f1680
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2022-11-02 23:36:11 +0300

    app.js changed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const re =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 console.log(inputs);
-trialBtn.addEventListener("click", e => {
+trialBtn.addEventListener("submit", e => {
   e.preventDefault();
   inputs.forEach(input => {
     if (input.value === "") {
