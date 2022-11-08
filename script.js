d90486e9966e7be37fc18882f13dd132bf7bdc98
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2022-11-08 16:25:44 +0300

    dynamic bars added

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ async function getData() {
   createChart(data);
 }
 
+getData();
+
 const date = new Date();
 
 function createChart(data) {
@@ -20,25 +22,29 @@ function createChart(data) {
 
     chartCol.innerHTML = `<div style="height:${Math.ceil(
       amount * 2.865
-    )}px" class="chart w-[33px] bg-${
+    )}px" class="chart-col w-[33px] bg-${
       idx === date.getDay() - 1 ? "cyan" : "softred"
-    } rounded-sm hover:transition-colors lg:w-[50px] lg:rounded-md lg:relative hover:opacity-75"></div>
-    <div class="text-mediumbrown text-xs mt-2 lg:text-base lg:tracking-tight ">${day}</div>`;
+    } rounded-sm hover:transition-colors lg:w-[50px] lg:rounded-md lg:relative hover:opacity-75" data-price="${amount}"></div>
+    <div class="text-mediumbrown text-xs mt-2 lg:text-base lg:tracking-tight">${day}</div>`;
 
     chart.appendChild(chartCol);
   });
 }
 
-// chart.addEventListener("click", showPrice);
+chart.addEventListener("click", addPriceTag);
 
-// function showPrice(e) {
-//   if (e.target.classList.contains("chart")) {
-//     let price = document.createElement("div");
-//     price.innerText = `${dataset.price}`;
-//     console.log(price);
-//   } else {
-//     return;
-//   }
-// }
-
-getData();
+function addPriceTag(e) {
+  if (e.target.classList.contains("chart-col") && window.innerWidth > 1023) {
+    let price = document.createElement("div");
+    price.id = "price";
+    price.className =
+      "flex items-center justify-center p-[10px] bg-darkbrown absolute -top-12 -left-[14px] text-verypaleorange rounded-md font-bold";
+    price.innerText = `$${e.target.dataset.price}`;
+    e.target.appendChild(price);
+    setTimeout(() => {
+      price.remove();
+    }, 2000);
+  } else {
+    return;
+  }
+}
