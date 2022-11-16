eedb51bb1d58a33601842123c30b025eb84e7a99
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2022-11-16 23:27:43 +0300

    mobile menu completed

---
+++ app/js/script.js
@@ -0,0 +1,38 @@
+const dropdowns = document.querySelectorAll(".navbar__dropdown");
+const hamburgerBtn = document.querySelector(".navbar__hamburger-btn");
+const closeBtn = document.querySelector(".navbar__close-btn");
+const navbarLinks = document.querySelector(".navbar__links");
+const dropdownLinks = document.querySelectorAll(".toggle");
+const dropdownIcons = document.querySelectorAll(".dropdown-icon");
+
+dropdowns.forEach(dropdown => {
+  dropdown.previousElementSibling.addEventListener("click", () => {
+    dropdown.classList.toggle("block");
+  });
+});
+
+hamburgerBtn.addEventListener("click", () => {
+  navbarLinks.classList.add("block");
+  document.body.classList.add("black-background");
+  hamburgerBtn.classList.add("none");
+  closeBtn.classList.add("block");
+});
+
+closeBtn.addEventListener("click", () => {
+  navbarLinks.classList.remove("block");
+  document.body.classList.remove("black-background");
+  hamburgerBtn.classList.remove("none");
+  closeBtn.classList.remove("block");
+});
+
+dropdownLinks.forEach((dropdownLink, index) => {
+  let toggle = true;
+  dropdownLink.addEventListener("click", () => {
+    toggle = !toggle;
+    if (toggle) {
+      dropdownIcons[index].src = "./images/icon-arrow-down.svg";
+    } else {
+      dropdownIcons[index].src = "./images/icon-arrow-up.svg";
+    }
+  });
+});
