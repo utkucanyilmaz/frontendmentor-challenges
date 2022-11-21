a0da84fd2c976054a7470e65ac894e2f9e4547b2
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2022-11-21 14:15:06 +0300

    overflow fixed

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const menuBtn = document.getElementById("menu-btn");
 const navList = document.querySelector(".nav-list");
 const menuIcon = document.querySelector(".menu-icon");
 const closeIcon = document.querySelector(".close-icon");
-console.log(menuBtn, navList);
 
 menuBtn.addEventListener("click", () => {
   navList.classList.toggle("block");
   menuIcon.classList.toggle("block");
   closeIcon.classList.toggle("block");
   document.body.classList.toggle("black-bg");
+  document.body.classList.toggle("overflow");
 });
