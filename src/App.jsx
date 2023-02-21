85386ad5ddb613d981ebee8b0477cc808626f214
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-02-21 23:01:17 +0300

    Add SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="h-screen bg-neutral-dark-blue-700 font-nunito">
       <Header />
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaSearch, FaTimes } from "react-icons/fa";
+
+function SearchBar() {
+  return (
+    <div className="relative mt-10 ml-10 flex h-14 w-[480px] items-center text-white">
+      <label className="absolute left-6" htmlFor="search-bar">
+        <FaSearch />
+      </label>
+      <input
+        id="search-bar"
+        type="text"
+        placeholder="Search for a country..."
+        className="h-full w-full rounded-md bg-neutral-dark-blue-500 py-4 pl-16 text-sm caret-white placeholder:text-white focus:placeholder:text-transparent"
+      />
+
+      <button className="absolute right-3 p-1">
+        <FaTimes />
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
