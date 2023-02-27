eb071a0bc9b96d7097dcbaf712da8213a550ebe5
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-02-27 23:04:46 +0300

    Add clearSearch func

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import { useRef } from "react";
 import { FaSearch, FaTimes } from "react-icons/fa";
 import { useFilter } from "../context/FilterContext";
 
 function SearchBar() {
+  const searchBar = useRef();
+
   const { setFiltered, countries } = useFilter();
 
   const handleChange = ({ target }) => {
@@ -16,6 +18,11 @@ function SearchBar() {
     );
   };
 
+  const clearSearch = () => {
+    searchBar.current.value = "";
+    setFiltered(countries);
+  };
+
   return (
     <div className="relative mx-auto flex h-14 w-[calc(100%-2rem)] items-center text-neutral-dark-blue-800 shadow-md dark:text-white md:mx-0 md:w-[480px] ">
       <label className="absolute left-4 md:left-6" htmlFor="search-bar">
@@ -23,6 +30,7 @@ function SearchBar() {
       </label>
 
       <input
+        ref={searchBar}
         id="search-bar"
         type="text"
         onChange={handleChange}
@@ -30,8 +38,8 @@ function SearchBar() {
         className="h-full w-full rounded bg-white py-4 pl-12 text-sm caret-neutral-dark-blue-800 placeholder:text-neutral-gray-500 focus:placeholder:text-transparent dark:bg-neutral-dark-blue-500 dark:caret-white dark:placeholder:text-white md:pl-16 "
       />
 
-      <button className="absolute right-3 p-1">
-        <FaTimes />
+      <button onClick={clearSearch} className="absolute right-3 p-1">
+        <FaTimes className="transition-transform hover:scale-[.8]" />
       </button>
     </div>
   );
