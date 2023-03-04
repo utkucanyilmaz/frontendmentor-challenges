a9fd85d611f007facad6732ca969e15da63e4511
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-03-04 23:43:09 +0300

    Fix remain filtered after page change

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
+import { useFilter } from "../context/FilterContext";
 
 function BackButton() {
+  const { setFiltered, countries } = useFilter();
+
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+    setFiltered(countries);
+  };
+
   return (
     <button
-      onClick={() => navigate(-1)}
+      onClick={handleClick}
       className="flex items-center gap-x-2 rounded bg-white px-6 py-2 text-sm text-neutral-dark-blue-800 drop-shadow-lg hover:opacity-80 dark:bg-neutral-dark-blue-500 dark:text-white sm:px-10"
     >
       <FaArrowLeft />
