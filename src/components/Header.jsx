baed25236e1d35ac33a50910e63ac41652b9fae4
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-03-06 20:40:12 +0300

    Add Link to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ThemeButton from "./ThemeButton";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="bg-white shadow-md dark:bg-neutral-dark-blue-500 ">
       <div className="container mx-auto flex items-center justify-between py-7 px-4 md:px-0">
-        <h1 className="text-md font-bold text-neutral-dark-blue-800 dark:text-white sm:text-xl ">
-          Where in the world?
-        </h1>
+        <Link to={"/"}>
+          <h1 className="text-md font-bold text-neutral-dark-blue-800 dark:text-white sm:text-xl ">
+            Where in the world?
+          </h1>
+        </Link>
         <div>
           <ThemeButton />
         </div>
