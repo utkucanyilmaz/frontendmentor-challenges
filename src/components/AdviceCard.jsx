e3d16aa7d06f000f9cdf59ae3ca84a325cb5f102
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-03-21 00:19:15 +0300

    Add responsive design

--- src/components/AdviceCard.jsx
+++ src/components/AdviceCard.jsx
@@ -3,19 +3,27 @@ import DiceButton from "./DiceButton";
 
 function AdviceCard({ data, getAdvice }) {
   return (
-    <div className="p-12 flex flex-col items-center justify-center bg-dark-grayish-blue font-monrope rounded-2xl gap-y-6 relative mx-4">
-      <div className="text-sm uppercase tracking-[.30em] font-medium text-neon-green text-center">
+    <div className="px-6 py-10 sm:p-12 flex flex-col items-center justify-center bg-dark-grayish-blue font-monrope rounded-xl sm:rounded-2xl gap-y-6 relative mx-4 sm:mx-0">
+      <div className="text-xs sm:text-sm uppercase tracking-[.36em] font-medium text-neon-green text-center">
         Advice #{data.slip.id}
       </div>
-      <p className="text-light-cyan font-medium text-[28px] max-w-[25ch] text-center leading-[43px]">
+
+      <p className="text-light-cyan font-medium text-2xl sm:text-[28px] max-w-[20ch] sm:max-w-[25ch] text-center leading-8 sm:leading-[42px]">
         “{data.slip.advice}”
       </p>
 
-      <img
-        src="images/pattern-divider-desktop.svg"
-        alt="Pattern Divider"
-        className="mb-6"
-      />
+      <picture>
+        <source
+          media="(max-width: 639px)"
+          srcSet="images/pattern-divider-mobile.svg"
+          alt="Pattern Divider"
+        />
+        <img
+          src="images/pattern-divider-desktop.svg"
+          alt="Pattern Divider"
+          className="mb-6"
+        />
+      </picture>
 
       <DiceButton getAdvice={getAdvice} />
     </div>
--- src/components/DiceButton.jsx
+++ src/components/DiceButton.jsx
@@ -4,7 +4,7 @@ function DiceButton({ getAdvice }) {
   return (
     <button
       onClick={getAdvice}
-      className="rounded-full flex items-center justify-center p-6 bg-neon-green hover:shadow-neon-green hover:shadow-[0_0_36px_0] transition-shadow absolute bottom-0 translate-y-1/2"
+      className="rounded-full flex items-center justify-center p-5 bg-neon-green hover:shadow-neon-green hover:shadow-[0_0_36px_0] transition-shadow absolute bottom-0 translate-y-1/2"
     >
       <img src="/images/icon-dice.svg" alt="Random advice button logo" />
     </button>
