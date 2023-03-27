3cef0bf2ebf49c7f320d1fa059cedc324108b0f5
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-03-27 22:21:15 +0300

    Add Header Update styling, hover states

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import Header from "./components/Header";
+import Destination from "./pages/Destination";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="h-screen bg-home-desktop bg-cover">
+    <div className="h-screen bg-destination-desktop bg-cover bg-center overflow-hidden">
+      <div className="absolute h-24 w-[57%] right-0 backdrop-blur-lg bg-white bg-opacity-5 mt-12"></div>
       <div className="container mx-auto text-lg">
-        <Home />
+        <Header />
+        {/* <Home /> */}
+        <Destination />
       </div>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Header() {
+  const navItems = ["home", "destination", "crew", "technology"];
+
+  return (
+    <header className="flex items-center justify-between h-48 relative mr-16">
+      <div className="h-12 w-12">
+        <img
+          className="h-full w-full"
+          src="assets/shared/logo.svg"
+          alt="Logo"
+        />
+      </div>
+
+      <div className="w-[38%] h-[1px] bg-hover mr-auto ml-16 z-10 opacity-25"></div>
+
+      <nav className="font-barlow-condensed flex gap-x-12 text-white">
+        {navItems.map((item, idx) => (
+          <a
+            key={idx}
+            href="#"
+            className="flex gap-x-3 uppercase py-8 border-b-[3px] hover:border-hover tracking-[2.7px] text-base"
+          >
+            <span className="font-bold">0{idx}</span>
+            {item}
+          </a>
+        ))}
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
