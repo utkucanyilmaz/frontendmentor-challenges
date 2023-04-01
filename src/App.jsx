19b48db514ae54bf400ac0b74283b4f96f18cb67
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-04-01 23:29:03 +0300

    Add framer-motion animations

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./pages/Home";
 import Crew from "./pages/Crew";
 import Technology from "./pages/Technology";
 import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, MotionConfig } from "framer-motion";
+import AnimatePages from "./components/AnimatePages";
 
 function App() {
   let location = useLocation().pathname;
@@ -50,25 +52,29 @@ function App() {
   }
 
   return (
-    <div
-      className={`${bgImage} bg-no-repeat bg-center bg-cover h-screen overflow-x-hidden xl:overflow-hidden pb-10 relative`}
-    >
-      {windowSize.innerWidth >= 768 && (
-        <div className="absolute h-24 w-[57%] lg:w-[45%] xl:w-[57%] right-0 backdrop-blur-lg bg-white bg-opacity-5 xl:mt-12"></div>
-      )}
+    <MotionConfig reducedMotion="user">
+      <div
+        className={`${bgImage} bg-no-repeat bg-center bg-cover h-screen overflow-x-hidden xl:overflow-hidden pb-10 relative`}
+      >
+        {windowSize.innerWidth >= 768 && (
+          <div className="absolute h-24 w-[57%] lg:w-[45%] xl:w-[57%] right-0 backdrop-blur-lg bg-white bg-opacity-5 xl:mt-12"></div>
+        )}
 
-      {windowSize.innerWidth < 768 && <Header windowSize={windowSize} />}
+        {windowSize.innerWidth < 768 && <Header windowSize={windowSize} />}
 
-      <div className="container mx-auto text-lg">
-        {windowSize.innerWidth >= 768 && <Header windowSize={windowSize} />}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destination" element={<Destination />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
-        </Routes>
+        <div className="container mx-auto text-lg">
+          {windowSize.innerWidth >= 768 && <Header windowSize={windowSize} />}
+          <AnimatePresence mode="wait">
+            <Routes key={location} location={location}>
+              <Route path="/" element={<Home />} />
+              <Route path="/destination" element={<Destination />} />
+              <Route path="/crew" element={<Crew />} />
+              <Route path="/technology" element={<Technology />} />
+            </Routes>
+          </AnimatePresence>
+        </div>
       </div>
-    </div>
+    </MotionConfig>
   );
 }
 
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,5 +1,14 @@
 import { useState, useEffect } from "react";
 import data from "../data.json";
+import AnimatePages from "../components/AnimatePages";
+import { motion } from "framer-motion";
+
+const animations = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+  transition: { duration: 0.3 },
+};
 
 function Technology() {
   const [technology, setTechnology] = useState(null);
@@ -17,57 +26,70 @@ function Technology() {
   }, []);
 
   return (
-    <main className="text-white items-center xl:items-start xl:ml-16 xl:mt-20 flex flex-col min-h-screen">
-      {technology && (
-        <>
-          <div className="ml-8 md:self-start md:mt-10 xl:mt-0 uppercase font-barlow-condensed flex gap-x-5 xl:gap-x-7 tracking-nav md:tracking-header text-base md:text-xl xl:text-28">
-            <span className="opacity-25">03</span>Space Launch 101
-          </div>
+    <AnimatePages>
+      <main className="text-white items-center xl:items-start xl:ml-16 xl:mt-20 flex flex-col min-h-screen">
+        {technology && (
+          <>
+            <div className="md:ml-8 md:self-start md:mt-10 xl:mt-0 uppercase font-barlow-condensed flex gap-x-5 xl:gap-x-7 tracking-nav md:tracking-header text-base md:text-xl xl:text-28">
+              <span className="opacity-25">03</span>Space Launch 101
+            </div>
 
-          <div className="mt-7 flex w-full flex-col-reverse xl:flex-row items-center justify-center xl:justify-between">
-            <div className="flex gap-x-20 flex-col text-center xl:text-start xl:flex-row">
-              <div className="flex flex-row xl:flex-col justify-center gap-4 xl:gap-8 mb-7 md:mb-11 xl:mb-0">
-                {techButtons.map((_, idx) => (
-                  <button
-                    key={idx}
-                    onClick={() => handleTechClick(idx)}
-                    className={`w-[40px] h-[40px] md:w-[60px] md:h-[60px] xl:w-20 xl:h-20 flex items-center border border-transparent justify-center text-base md:text-2xl xl:text-3xl font-bellefair rounded-full cursor-pointer transition-colors ${
-                      active === idx
-                        ? "bg-white text-dark-blue"
-                        : "border-white border-opacity-50 hover:border-opacity-100 bg-transparent"
-                    }`}
-                  >
-                    {idx + 1}
-                  </button>
-                ))}
-              </div>
-
-              <div>
-                <div className="font-barlow-condensed text-tealish-blue text-sm md:text-base tracking-nav mb-2 md:mb-4 xl:mb-3">
-                  THE TERMINOLOGY…
+            <div className="mt-7 flex w-full flex-col-reverse xl:flex-row items-center justify-center xl:justify-between">
+              <div className="flex gap-x-20 flex-col text-center xl:text-start xl:flex-row">
+                <div className="flex flex-row xl:flex-col justify-center gap-4 xl:gap-8 mb-7 md:mb-11 xl:mb-0">
+                  {techButtons.map((_, idx) => (
+                    <button
+                      key={idx}
+                      onClick={() => handleTechClick(idx)}
+                      className={`w-[40px] h-[40px] md:w-[60px] md:h-[60px] xl:w-20 xl:h-20 flex items-center border border-transparent justify-center text-base md:text-2xl xl:text-3xl font-bellefair rounded-full cursor-pointer transition-colors ${
+                        active === idx
+                          ? "bg-white text-dark-blue"
+                          : "border-white border-opacity-50 hover:border-opacity-100 bg-transparent"
+                      }`}
+                    >
+                      {idx + 1}
+                    </button>
+                  ))}
                 </div>
-                <h1 className="text-2xl leading-[28px] md:text-[40px] md:leading-[46px] xl:text-[56px] xl:leading-[64px] uppercase mb-4">
-                  {technology.name}
-                </h1>
-                <p className="text-[15px] leading-[25px] text-tealish-blue max-w-[327px] md:max-w-[458px] xl:max-w-[42ch] text-base xl:text-lg md:leading-7 xl:leading-8">
-                  {technology.description}
-                </p>
-              </div>
-            </div>
 
-            <div className="h-auto xl:w-auto mb-14 xl:mb-0">
-              <picture>
-                <source
-                  media="(max-width: 1279px)"
-                  srcSet={technology.images.landscape}
-                />
-                <img src={technology.images.portrait} alt={technology.name} />
-              </picture>
+                <motion.div
+                  variants={animations}
+                  initial="initial"
+                  animate="animate"
+                  key={technology.name}
+                >
+                  <div className="font-barlow-condensed text-tealish-blue text-sm md:text-base tracking-nav mb-2 md:mb-4 xl:mb-3">
+                    THE TERMINOLOGY…
+                  </div>
+                  <h1 className="text-2xl leading-[28px] md:text-[40px] md:leading-[46px] xl:text-[56px] xl:leading-[64px] uppercase mb-4">
+                    {technology.name}
+                  </h1>
+                  <p className="text-[15px] leading-[25px] text-tealish-blue max-w-[327px] md:max-w-[458px] xl:max-w-[42ch] text-base xl:text-lg md:leading-7 xl:leading-8">
+                    {technology.description}
+                  </p>
+                </motion.div>
+              </div>
+
+              <motion.div
+                variants={animations}
+                initial="initial"
+                animate="animate"
+                key={technology.name}
+                className="h-auto xl:w-auto mb-14 xl:mb-0"
+              >
+                <picture>
+                  <source
+                    media="(max-width: 1279px)"
+                    srcSet={technology.images.landscape}
+                  />
+                  <img src={technology.images.portrait} alt={technology.name} />
+                </picture>
+              </motion.div>
             </div>
-          </div>
-        </>
-      )}
-    </main>
+          </>
+        )}
+      </main>
+    </AnimatePages>
   );
 }
 
