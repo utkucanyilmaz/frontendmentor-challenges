7047f1a55d29517c166b609a25ddcb1aecec20cc
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-05-17 23:31:12 +0300

    Add responsive design

--- main.js
+++ main.js
@@ -3,25 +3,29 @@ const sliderItems = [
     title: "Discover innovative ways to decorate",
     descp:
       "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-    imageSrc: "/images/mobile-image-hero-1.jpg",
+    mobileImageSrc: "/images/mobile-image-hero-1.jpg",
+    desktopImageSrc: "/images/desktop-image-hero-1.jpg",
   },
   {
     title: "We are available all across the globe",
     descp:
       "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today..",
-    imageSrc: "/images/mobile-image-hero-2.jpg",
+    mobileImageSrc: "/images/mobile-image-hero-2.jpg",
+    desktopImageSrc: "/images/desktop-image-hero-2.jpg",
   },
   {
     title: "Manufactured with the best materials",
     descp:
       "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-    imageSrc: "/images/mobile-image-hero-3.jpg",
+    mobileImageSrc: "/images/mobile-image-hero-3.jpg",
+    desktopImageSrc: "/images/desktop-image-hero-3.jpg",
   },
 ];
 
 let sourceIndex = 0;
 
-const heroImage = document.getElementById("hero-image");
+const mobileImageHero = document.getElementById("mobile-image-hero");
+const desktopImageHero = document.getElementById("desktop-image-hero");
 const title = document.getElementById("title");
 const descp = document.getElementById("descp");
 const sliderLeft = document.getElementById("slider-left");
@@ -29,6 +33,7 @@ const sliderRight = document.getElementById("slider-right");
 const navBtn = document.getElementById("nav-btn");
 const navCloseBtn = document.getElementById("nav-close-btn");
 const navbar = document.getElementById("navbar");
+const productDescp = document.getElementById("product-descp");
 
 const changeSliderItem = isBtnLeft => {
   if (isBtnLeft && sourceIndex > 0) {
@@ -41,7 +46,8 @@ const changeSliderItem = isBtnLeft => {
 
   title.innerText = sliderItems[sourceIndex].title;
   descp.innerText = sliderItems[sourceIndex].descp;
-  heroImage.src = sliderItems[sourceIndex].imageSrc;
+  desktopImageHero.src = sliderItems[sourceIndex].desktopImageSrc;
+  mobileImageHero.srcset = sliderItems[sourceIndex].mobileImageSrc;
 };
 
 navBtn.addEventListener("click", () => {
